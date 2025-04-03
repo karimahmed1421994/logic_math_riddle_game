cfa7f64956d9c0b5464b04c7201c0a722cad0ab7
karimahmed1421994 <karim14294@gmail.com>
2025-04-03 19:58:53 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: Center(child: Text('Hello Flutter!')))));
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Logic Riddle Game')),
+      body: Center(child: Text('Welcome to Logic Riddle Game!')),
+    ),
+  ));
 }
